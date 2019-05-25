e3db0ad27ff0a81c2a73e8fa051ebc928a3cc45c
wanglihui <617192629@qq.com>
2019-05-25 19:58:15 +0800

    把本地工程添加到git

---
+++ src/cn/itcast/Hello.java
@@ -0,0 +1,9 @@
+package cn.itcast;
+
+public class Hello {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("第"+(i+1)+"次"+"哈哈");
+        }
+    }
+}
